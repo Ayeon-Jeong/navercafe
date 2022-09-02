9d23af08aa022d5589328093302bd022276b9754
Ayeon-Jeong <57283339+Ayeon-Jeong@users.noreply.github.com>
2022-09-02 14:42:47 +0900

    login page

--- src/main/java/com/navercafe/project1/controller/LoginController.java
+++ src/main/java/com/navercafe/project1/controller/LoginController.java
@@ -1,13 +1,24 @@
 package com.navercafe.project1.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class LoginController {
 
     @GetMapping("/login.do")
-    public String loginController(){
+    public String loginController() {
+        return "login";
+
+    }
+
+    @GetMapping("/print.do")
+    public String printController(HttpServletRequest request) {
+        System.out.println(request.getParameter("username"));
+        System.out.println(request.getParameter("password"));
         return "login";
     }
 }
